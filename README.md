c971b66ddfea75427c62621b9a11f9a1378fe175
Morten Henriksen <mh@gi2.dk>
2015-08-25 02:17:52 +0200

    1.2.4-rc.1 Meteor 1.2-rc.4 test

---
+++ README.md
@@ -0,0 +1,6 @@
+raix:localforage-websql
+=======================
+
+Offline storage, improved. Wraps IndexedDB, WebSQL, or localStorage using a simple but powerful API. On cordova we also support SQLite.
+
+[Mozilla's LocalForage](https://github.com/mozilla/localForage)
--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'raix:localforage-websql',
-  version: '1.2.4',
+  version: '1.2.4-rc.1',
   summary: "Offline storage driver WebSQL"
 });
 
 Package.on_use(function (api) {
-  // api.versionsFrom('1.2');
+  api.versionsFrom('METEOR@1.2-rc.4');
   api.use('ecmascript', 'client');
-  api.use('raix:localforage-serializer@1.2.4', 'client');
+  api.use('raix:localforage-serializer@1.2.4-rc.1', 'client');
 
   api.addFiles('websql.js', 'client');
   api.export('webSQLStorage', 'client');
